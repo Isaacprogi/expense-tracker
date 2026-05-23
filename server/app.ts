0ed112f0a508c907b47da1a62920a1743795927e
isaacprogi <isaaconyes80@gmail.com>
2026-05-23 17:50:11 +0100

    feat: add kinde auth

--- server/app.ts
+++ server/app.ts
@@ -3,11 +3,15 @@ import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
 import { serveStatic } from "hono/bun";
 const app = new Hono()
+import { authRoute } from "./routes/auth";
 
 app.use("*", logger())
 
 const apiRoutes = app.basePath("/api")
 .route("/expenses",expensesRoute)
+.route('/',authRoute)
+
+console.log("KINDE_REDIRECT_URI:", process.env.KINDE_REDIRECT_URI);
 
 // Static files (serve frontend or assets)
 app.get("*", serveStatic({ root: "./frontend/dist" }));
